29f0087d3a4796c034c9973e7756797a17836fc4
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-06-22 23:42:27 +0800

    Create navbar's initial design

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@components/Feed'
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -8,7 +10,8 @@ const Home = () => {
       </h1>
       <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
 
-      {/* Feed */}
+      
+      <Feed />
     </section>
   )
 }
